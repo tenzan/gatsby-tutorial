90a1506d08b0c5a14e900a7306e66cc1b8271d00
Askar Karasaev <askar.karasaev@gmail.com>
2020-10-21 16:46:15 +0900

    2-12 Navbar

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'gatsby'
+
+const blog = () => {
+    return (
+        <div>
+            <h1>This is our blog page</h1>
+            <Link to="/">Home page</Link>
+        </div>
+    )
+}
+
+export default blog
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import {Link} from 'gatsby'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>Hello world!
+    <div>
+      <Navbar></Navbar>
+      <Link to="/blog/">blog page</Link>
+    </div>
+  </div>
 }
