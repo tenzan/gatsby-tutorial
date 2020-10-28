f3e602a00b52701708dd46b192529584f28b3f89
Askar Karasaev <askar.karasaev@gmail.com>
2020-10-28 23:02:40 +0900

    lesson 2-61 Gatsby-Node

---
+++ src/templates/productTemplate.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const productTemplate = props => {
+  return (
+    <div>
+      <h1>hello from product template</h1>
+    </div>
+  );
+};
+
+export default productTemplate;
